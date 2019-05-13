47899c555434f34b507f6fc766c0bbeccb0480a6
samuele <samuelealbani@gmail.com>
2019-05-14 00:32:31 +0200

    started vectors

---
+++ Section3/vectorTest.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout; using std::cin; using std::endl;
+using std::vector;
+using std::string;
+
+int main(){
+  // vector is not a type, it's a container of objects of any type
+  // initializations
+  // list of initial elements
+  vector<int> first_vector{0, 23, 14};
+
+  // set the initial number of elements
+  vector<int> second_vector(10);
+
+  // set the initial number of elements, and set initial values of each
+  vector<int> third_vector(3, 14);
+  vector<string> fourth_vector(5, "hello!");
+
+  cout << "first vector = {";
+  for (int el : first_vector){
+    cout << el << " ";
+  }
+  cout << "}" << endl;
+
+  for(string salutos : fourth_vector){
+    cout << salutos << " ";
+  }
+  cout <<"\n";
+  /*
+  cout << first_vector << endl;
+  cout << second_vector << endl;
+  cout << third_vector << endl;
+  cout << fourth_vector << endl;
+  */
+
+  return 0;
+}
